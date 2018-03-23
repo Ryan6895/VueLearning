fd07dd245aabf0536f72e7bb8fbb6b857d890b06
Ryan Clark <Rymaster95@gmail.com>
2018-03-23 17:18:24 -0400

    added webpack

---
+++ build/webpack.base.config.js
@@ -0,0 +1,12 @@
+const path = require("path");
+
+const config = {
+  entry: {
+    app: path.resolve(__dirname,"../src/client-entry.js")
+  }
+  output: path.resolve(__dirname, "../dist"),
+  publicPath: "/",
+  filename: "assets/js/[name].js"
+};
+
+module.exports = config;
