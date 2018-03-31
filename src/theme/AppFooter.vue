cbab8c9b58214fbc8f8b1e47b05b22ec2ac68d03
Ryan Clark <Rymaster95@gmail.com>
2018-03-30 21:01:34 -0400

    added routing to app

---
+++ src/theme/AppFooter.vue
@@ -0,0 +1,18 @@
+<template lang="html">
+  <footer class="footer">
+    <div class="container">
+      <div class="content has-text-centered">
+        Follow us on test 2
+        <a href="https://twitter.com/bstavroulakis" target="_blank">Twitter</a>
+      </div>
+    </div>
+  </footer>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="css">
+</style>
---
+++ src/theme/AppHeader.vue
@@ -0,0 +1,17 @@
+<template lang="html">
+  <nav class="nav has-shadow">
+    <div class="container">
+      <a href="/">
+          <img src="http://bit.ly/vue-img" alt="Vue SPA" />
+        </a>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="css">
+</style>
